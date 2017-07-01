f1d68b383343767c698b9bfab1b2357f902f297b
Robert Zobrist <zobrist.robert.480@gmail.com>
2017-07-01 14:50:40 -0700

    changes

--- clozeCard.js
+++ clozeCard.js
@@ -1,9 +1,7 @@
 // cloze card constructor
-<<<<<<< HEAD
+
 function ClozeCard(partial, cloze) {
-=======
-function ClozeCard(text, cloze) {
->>>>>>> master
+
      this.cloze = cloze;
      this.partial = partial;
 
@@ -14,8 +12,4 @@ ClozeCard.prototype.fullText = function(cloze, partial) {
      return this.cloze + " " + this.partial;
 };
 
-<<<<<<< HEAD
 module.exports = ClozeCard;
-=======
-module.exports = ClozeCard;
->>>>>>> master
--- flashcard.js
+++ flashcard.js
@@ -12,7 +12,8 @@ var count;
 var limit;
 var n;
 // initial game setup
-<<<<<<< HEAD
+
+
 function cards() {
      inquirer.prompt([{
                name: "command",
@@ -50,52 +51,9 @@ function cards() {
 
      });
 }
+
 // get info function that collects data
 function getInfo(log) {
-=======
-function cards(){
-inquirer.prompt([{
-          name: "name",
-          message: "Hello, what is your name?"
-     }, {
-          type: "password",
-          message: "Please set your password",
-          name: "password"
-     }, {
-          type: "confirm",
-          message: "Are you sure:",
-          name: "confirm",
-          default: true
-     }, {
-          name: "commad",
-          type: "list",
-          message: "What would you like to do today?",
-          choices: ["Create Basic Flashcards", "Create ClozeCards", "Study my basic flashcards", "Study my ClozeCards", "Quit"]
-     }
-
-]).then(function(userResponse) {
-
-     if (userResponse.commad === "Quit") {
-          console.log("user Quit");
-     } else {
-          switch (userResponse.commad) {
-               case "Create Basic Flashcards":
-                    getInfo();
-                    break;
-
-               case "Study my basic flashcards":
-                    basicQuiz(0);
-                    break;
-
-          }
-     }
-
-
-});
-}
-// get info function that collects data
-function getInfo(basicCard) {
->>>>>>> master
 
      var front;
      var back;
@@ -104,20 +62,13 @@ function getInfo(basicCard) {
 
 
      if (count === 0) {
-<<<<<<< HEAD
           limitPrompt(log);
      }
      // sets limit on how many flashcards user will create in this session
 
+
+
      function limitPrompt(log) {
-=======
-          limitPrompt();
-     }
-     // sets limit on how many flashcards user will create in this session
-
-     function limitPrompt() {
->>>>>>> master
-
           inquirer.prompt([{
                type: "input",
                name: "limit",
@@ -127,61 +78,51 @@ function getInfo(basicCard) {
 
           .then(function(answers) {
                limit = parseInt(answers.limit);
-<<<<<<< HEAD
-               cardData(log);
-=======
-               cardData();
->>>>>>> master
+               cardData(log, limit);
+
           });
      }
 }
 // collects data from user 
-<<<<<<< HEAD
-function cardData(log) {
+
+
+function cardData(log, limit) {
      if (log === 'basiclog.txt') {
+
           inquirer.prompt([{
-=======
-function cardData() {
-     inquirer.prompt([{
->>>>>>> master
-
-               type: "input",
-               name: "front",
-               message: "Please enter the front of the flashcard, the question?"
-          }, {
-               type: "input",
-               name: "back",
-               message: "Now enter the back of the flashcard, the answer?"
-
-<<<<<<< HEAD
-          }]).then(function(answers2) {
-=======
-          }])
-          .then(function(answers2) {
->>>>>>> master
-               let front = answers2.front;
-               let back = answers2.back;
-
-               // creates flashcard wirh user inputs, then pushes new card to array
-               // increments count by one every time card is created
-               let newCard = new BasicCard(answers2.front, answers2.back);
-               simpleLibrary.push(newCard);
-               count++;
-
-               // if count is less than limit, keep creating cards
-               if (count < limit) {
-<<<<<<< HEAD
-                    cardData(log);
-
-               }
-               // if limit has been reached, log array of flashcards to external file for later use
-               else if (count == limit) {
-                    writeCards(log);
+                    type: "input",
+                    name: "front",
+                    message: "Please enter the front of the flashcard, the question?"
+               }, {
+                    type: "input",
+                    name: "back",
+                    message: "Now enter the back of the flashcard, the answer?"
 
 
-               }
+               }])
+               .then(function(answers2) {
+                    let front = answers2.front;
+                    let back = answers2.back;
 
-          });
+                    // creates flashcard wirh user inputs, then pushes new card to array
+                    // increments count by one every time card is created
+                    let newCard = new BasicCard(answers2.front, answers2.back);
+                    simpleLibrary.push(newCard);
+                    count++;
+
+                    // if count is less than limit, keep creating cards
+                    if (count < limit) {
+                         cardData(log, limit);
+
+                    }
+                    // if limit has been reached, log array of flashcards to external file for later use
+                    else if (count == limit) {
+                         writeCards(log);
+
+
+                    }
+
+               });
 
      } else {
           inquirer.prompt([{
@@ -206,26 +147,19 @@ function cardData() {
 
                // if count is less than limit, keep creating cards
                if (count < limit) {
-                    cardData(log);
-=======
-                    cardData();
->>>>>>> master
+                    cardData(log, limit);
+
 
                }
                // if limit has been reached, log array of flashcards to external file for later use
                else if (count == limit) {
-<<<<<<< HEAD
                     writeCards(log);
-=======
-                    writeCards();
->>>>>>> master
 
 
                }
 
           });
 
-<<<<<<< HEAD
 
      }
 
@@ -235,14 +169,9 @@ function writeCards(log) {
 
      simpleLibrary = JSON.stringify(simpleLibrary);
      fs.writeFile(log, simpleLibrary, function(err) {
-=======
-}
 
-function writeCards() {
 
-     simpleLibrary = JSON.stringify(simpleLibrary);
-     fs.appendFile("./log.txt", simpleLibrary, function(err) {
->>>>>>> master
+
           // If the code experiences any errors it will log the error to the console.
           if (err) {
                return console.log(err);
@@ -253,76 +182,37 @@ function writeCards() {
      });
 }
 
-<<<<<<< HEAD
-function readCard(log) {
-     let basicCardArr = [];
 
-     // Otherwise, it will print: "log.txt was updated!"
-     fs.readFile(log, "utf8", function(error, data) {
-=======
-function readCard() {
-     let basicCardArr = [];
 
-     // Otherwise, it will print: "log.txt was updated!"
-     fs.readFile("./log.txt", "utf8", function(error, data) {
->>>>>>> master
-
-          // If the code experiences any errors it will log the error to the console.
-          if (error) {
-               return console.log(error);
-          }
-          // Then split it by commas (to make it more readable
-
-          var jsonData = JSON.parse(data);
-
-          for (let i = 0; i < jsonData.length; i++) {
-               basicCardArr.push(jsonData[i]);
-
-          }
-
-     });
-}
 
 function lastCard(wrongAnswers, correctAnswers, j) {
-<<<<<<< HEAD
-     
+
+
      if (wrongAnswers + correctAnswers >= j) {
 
           console.log('\n\n\n\nHere\'s your results: ');
           console.log('correct: ' + correctAnswers);
-          console.log('wrong: ' + wrongAnswers +"\n\n\n\n");
+          console.log('wrong: ' + wrongAnswers + "\n\n\n\n");
           correctAnswers = 0;
           wrongAnswers = 0;
           cards();
-=======
-     if ((wrongAnswers || correctAnswers >= j) || (wrongAnswers + correctAnswers >= j)) {
 
-          console.log('Here\'s your results: ');
-          console.log('correct: ' + correct);
-          console.log('wrong: ' + wrong);
-          correctAnswers = 0;
-          wrongAnswers = 0;
-          flashcards();
->>>>>>> master
+
      }
-
 };
 
 
-<<<<<<< HEAD
+
+
 function basicQuiz(n, log) {
-=======
-function basicQuiz(n) {
->>>>>>> master
+
      var gameAnswer;
      var gameQuestion;
      var gameCard;
 
-<<<<<<< HEAD
+
      fs.readFile(log, "utf8", function(error, data) {
-=======
-     fs.readFile("./log.txt", "utf8", function(error, data) {
->>>>>>> master
+
 
           let jsonData = JSON.parse(data);
           j = jsonData.length;
@@ -335,13 +225,11 @@ function basicQuiz(n) {
                     gameQuestion = gameCard.front;
                     gameAnswer = gameCard.back.toLowerCase();
 
-<<<<<<< HEAD
                } else {
                     gameCard = new ClozeCard(jsonData[n].partial, jsonData[n].cloze);
                     gameQuestion = gameCard.partial;
                     gameAnswer = gameCard.cloze.toLowerCase();
-=======
->>>>>>> master
+
                }
                inquirer.prompt([{
                     name: "question",
@@ -360,41 +248,23 @@ function basicQuiz(n) {
                          console.log("correct");
                          correctAnswers++;
                          n++;
-<<<<<<< HEAD
                          lastCard(wrongAnswers, correctAnswers, j);
                          basicQuiz(n, log);
-=======
-                         lastCard();
-                         basicQuiz(n);
->>>>>>> master
+
+
 
                     } else {
                          gameCard.showCorrectAnswer();
                          wrongAnswers++;
                          n++;
-<<<<<<< HEAD
+
                          lastCard(wrongAnswers, correctAnswers, j);
                          basicQuiz(n, log);
-                    }
-
+                    };
                });
-          }
-     });
-
-
-};
-
-
-cards();
-=======
-                         lastCard();
-                         basicQuiz(n);
-                    }
-
-               });
-
           };
      });
 };
+
+
 cards();
->>>>>>> master
