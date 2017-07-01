617ab888988f1e2abc876330b14872cd647a0873
Robert Zobrist <zobrist.robert.480@gmail.com>
2017-07-01 13:18:19 -0700

    merging two seperate attempts at application

--- clozeCard.js
+++ clozeCard.js
@@ -1,20 +1,13 @@
-const clz = process.argv[2];
-const part = process.argv[3];
+// cloze card constructor
+function ClozeCard(partial, cloze) {
+     this.cloze = cloze;
+     this.partial = partial;
 
-
-function ClozeCard(part, clz) {
-     this.cloze = clz;
-     this.partial = part;
- 
-}Person.prototype.name = function() {
-    return this.firstName + " " + this.lastName;
 };
 
 
-ClozeCard.prototype.fullText = function(cloze, partial){
-	return this.cloze + " " + this.partial;
-}
+ClozeCard.prototype.fullText = function(cloze, partial) {
+     return this.cloze + " " + this.partial;
+};
 
-console.log(partial);
-console.log(cloze);
-console.log(fullText);
+module.exports = ClozeCard;
